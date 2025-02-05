712802ce707f13348ae0b0df3cf829f30d1438da
Matai Moorfield <matai@moorfield.co.nz>
2025-02-05 21:21:08 +1300

    reCAPTCHA

--- script.js
+++ script.js
@@ -80,10 +80,10 @@ const handleForm = async (e) => {
     try {
         const response = await emailjs.sendForm('service_6m1t6ul', 'template_portfolio', form);
         console.log("Email sent successfully:", response);
-        alert('Email sent!');
+        alert("Email sent! We'll be in touch soon!");
     } catch (error) {
         console.error("Email send failed:", error);
-        alert('Failed to send email');
+        alert("Failed to send email. We're working on it!");
     }
 };
 
